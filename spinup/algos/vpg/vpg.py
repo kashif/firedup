9549d801b64789bd762fb6090d06a1a4c5c6caa2
Kashif Rasul <kashif.rasul@gmail.com>
2018-11-25 21:58:34 +0100

    upstream fixes

--- spinup/algos/vpg/vpg.py
+++ spinup/algos/vpg/vpg.py
@@ -123,6 +123,7 @@ def vpg(env_fn,
     local_steps_per_epoch = int(steps_per_epoch / num_procs())
     buf = VPGBuffer(obs_dim, act_dim, local_steps_per_epoch, gamma, lam)
 
+    # Optimizers
     train_pi = torch.optim.Adam(actor_critic.policy.parameters(),
                                 lr=pi_lr)
     train_v = torch.optim.Adam(actor_critic.value_function.parameters(),
@@ -212,3 +213,27 @@ def vpg(env_fn,
         logger.log_tabular('KL', average_only=True)
         logger.log_tabular('Time', time.time()-start_time)
         logger.dump_tabular()
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env', type=str, default='HalfCheetah-v2')
+    parser.add_argument('--hid', type=int, default=64)
+    parser.add_argument('--l', type=int, default=2)
+    parser.add_argument('--gamma', type=float, default=0.99)
+    parser.add_argument('--seed', '-s', type=int, default=0)
+    parser.add_argument('--cpu', type=int, default=4)
+    parser.add_argument('--steps', type=int, default=4000)
+    parser.add_argument('--epochs', type=int, default=50)
+    parser.add_argument('--exp_name', type=str, default='vpg')
+    args = parser.parse_args()
+
+    mpi_fork(args.cpu)  # run parallel code with mpi
+
+    from spinup.utils.run_utils import setup_logger_kwargs
+    logger_kwargs = setup_logger_kwargs(args.exp_name, args.seed)
+
+    vpg(lambda : gym.make(args.env), actor_critic=core.ActorCritic,
+        ac_kwargs=dict(hidden_sizes=[args.hid]*args.l), gamma=args.gamma,
+        seed=args.seed, steps_per_epoch=args.steps, epochs=args.epochs,
+        logger_kwargs=logger_kwargs)
