f1dfea2a6f7a8f32be35475bf826a7596a54c1e2
Kashif Rasul <kashif.rasul@zalando.de>
2018-12-12 16:50:52 +0100

    fixed class name

--- spinup/algos/ppo/ppo.py
+++ spinup/algos/ppo/ppo.py
@@ -351,7 +351,7 @@ if __name__ == '__main__':
     from spinup.utils.run_utils import setup_logger_kwargs
     logger_kwargs = setup_logger_kwargs(args.exp_name, args.seed)
 
-    ppo(lambda : gym.make(args.env), actor_critic=core.mlp_actor_critic,
+    ppo(lambda : gym.make(args.env), actor_critic=core.ActorCritic,
         ac_kwargs=dict(hidden_sizes=[args.hid]*args.l), gamma=args.gamma, 
         seed=args.seed, steps_per_epoch=args.steps, epochs=args.epochs,
         logger_kwargs=logger_kwargs)
