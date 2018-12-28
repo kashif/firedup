64bf36d2cd75e1c437e04325aeebabc9d60f0df2
Kashif Rasul <kashif.rasul@zalando.de>
2018-12-28 23:45:45 +0100

    initial ddpg

--- spinup/__init__.py
+++ spinup/__init__.py
@@ -2,5 +2,6 @@ from spinup.algos.vpg.vpg import vpg
 from spinup.algos.ppo.ppo import ppo
 from spinup.algos.sac.sac import sac
 from spinup.algos.td3.td3 import td3
+from spinup.algos.ddpg.ddpg import ddpg
 
 from spinup.utils.logx import Logger, EpochLogger
---
+++ spinup/algos/ddpg/core.py
@@ -0,0 +1,56 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from gym.spaces import Box, Discrete
+
+
+def count_vars(module):
+    return sum(p.numel() for p in module.parameters() if p.requires_grad)
+
+
+class MLP(nn.Module):
+    def __init__(self, layers, activation=torch.tanh, output_activation=None,
+                 output_scale=1):
+        super(MLP, self).__init__()
+        self.layers = nn.ModuleList()
+        self.activation = activation
+        self.output_activation = output_activation
+        self.output_scale = output_scale
+
+        gain = nn.init.calculate_gain(activation.__name__)
+        for i, layer in enumerate(layers[1:]):
+            self.layers.append(nn.Linear(layers[i], layer))
+            nn.init.zeros_(self.layers[i].bias)
+            nn.init.xavier_uniform_(self.layers[i].weight, gain)
+
+    def forward(self, inputs):
+        x = inputs
+        for layer in self.layers[:-1]:
+            x = self.activation(layer(x))
+        if self.output_activation is None:
+            x = self.layers[-1](x)
+        else:
+            x = self.output_activation(self.layers[-1](x))
+        return self.output_scale * x
+
+class ActorCritic(nn.Module):
+    def __init__(self, in_features, action_space,
+                 hidden_sizes=(400, 300),
+                 activation=torch.relu,
+                 output_activation=torch.tanh):
+        super(ActorCritic, self).__init__()
+
+        action_dim = action_space.shape[0]
+        action_scale = action_space.high[0]
+
+        self.policy = MLP(layers=[in_features]+list(hidden_sizes)+[action_dim],
+                          activation=activation, output_activation=output_activation, output_scale=action_scale)
+        self.q = MLP(layers=[in_features+action_dim]+list(hidden_sizes)+[1],
+                     activation=activation)
+
+    def forward(self, x, a):
+        pi = self.policy(x)
+        q = self.q(torch.cat((x, a), dim=1))
+        q_pi = self.q(torch.cat((x, pi), dim=1))
+
+        return pi, torch.squeeze(q), torch.squeeze(q_pi)
